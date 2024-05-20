4af3af72aaaa59a48510779f3f37e2ee49cdb569
chairul <chairul.amri@ebconnect.com>
2024-05-20 18:49:34 +0700

    [add] Codelab Navigation Drawer

--- state-pada-compose/MyNavDrawer(SideEffect)/app/src/main/java/com/learn/jetpack/mynavdrawer/MyNavDrawerApp.kt
+++ state-pada-compose/MyNavDrawer(SideEffect)/app/src/main/java/com/learn/jetpack/mynavdrawer/MyNavDrawerApp.kt
@@ -1,11 +1,48 @@
 package com.learn.jetpack.mynavdrawer
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.learn.jetpack.mynavdrawer.ui.theme.MyNavDrawerTheme
 
 @Composable
 fun MyNavDrawerApp() {
+    Scaffold(topBar = { MyTopBar(onMenuClick = {}) }) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text(text = stringResource(id = R.string.hello_world))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { onMenuClick() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(id = R.string.menu)
+                )
+            }
+        },
+        title = { Text(text = stringResource(id = R.string.app_name)) })
 }
 
 @Preview(showBackground = true)
