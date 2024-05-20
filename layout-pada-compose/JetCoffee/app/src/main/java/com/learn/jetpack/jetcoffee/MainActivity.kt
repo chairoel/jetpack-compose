df54e14059c55c5687906763fcd4779fc1598cd7
chairul <chairul.amri@ebconnect.com>
2024-05-20 11:06:10 +0700

    [add] Banner on Home Page

--- layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.learn.jetpack.jetcoffee.ui.components.Search
 import com.learn.jetpack.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +33,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
+fun Banner(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
+}
 
+@Composable
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetCoffeeAppPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
 }
--- layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/ui/components/Search.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/ui/components/Search.kt
@@ -1,7 +1,6 @@
 package com.learn.jetpack.jetcoffee.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
