d1df5264e040ce11d51cc8446d2e4f8fb8643ed2
chairul <chairul.amri@ebconnect.com>
2024-05-20 11:32:43 +0700

    [add] Categories on Home Page

--- layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +22,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.learn.jetpack.jetcoffee.model.dummyCategory
+import com.learn.jetpack.jetcoffee.ui.components.CategoryItem
 import com.learn.jetpack.jetcoffee.ui.components.Search
 import com.learn.jetpack.jetcoffee.ui.theme.JetCoffeeTheme
 
@@ -49,6 +55,27 @@ fun Banner(modifier: Modifier = Modifier) {
 fun JetCoffeeApp() {
     Column {
         Banner()
+    }
+}
+
+@Composable
+fun CategoryRow(modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+//        modifier = modifier.padding(16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
     }
 }
 
