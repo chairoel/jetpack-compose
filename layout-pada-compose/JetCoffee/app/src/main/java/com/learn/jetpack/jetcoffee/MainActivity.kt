e405d4bedfd5f6d806cba3dd21c49145070042c4
chairul <chairul.amri@ebconnect.com>
2024-05-20 13:29:20 +0700

    [add] Menu Sections on Home page

--- layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
+++ layout-pada-compose/JetCoffee/app/src/main/java/com/learn/jetpack/jetcoffee/MainActivity.kt
@@ -23,8 +23,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.learn.jetpack.jetcoffee.model.Menu
 import com.learn.jetpack.jetcoffee.model.dummyCategory
+import com.learn.jetpack.jetcoffee.model.dummyMenu
 import com.learn.jetpack.jetcoffee.ui.components.CategoryItem
+import com.learn.jetpack.jetcoffee.ui.components.MenuItem
 import com.learn.jetpack.jetcoffee.ui.components.Search
 import com.learn.jetpack.jetcoffee.ui.components.SectionText
 import com.learn.jetpack.jetcoffee.ui.theme.JetCoffeeTheme
@@ -66,8 +69,8 @@ fun JetCoffeeApp() {
 fun CategoryRow(modifier: Modifier = Modifier) {
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp)
-//        modifier = modifier.padding(16.dp)
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
     ) {
         items(dummyCategory, key = { it.textCategory }) { category ->
             CategoryItem(category)
@@ -80,6 +83,30 @@ fun CategoryRow(modifier: Modifier = Modifier) {
 fun CategoryRowPreview() {
     JetCoffeeTheme {
         CategoryRow()
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MenuRowPreview() {
+    JetCoffeeTheme {
+        MenuRow(listMenu = dummyMenu)
     }
 }
 
