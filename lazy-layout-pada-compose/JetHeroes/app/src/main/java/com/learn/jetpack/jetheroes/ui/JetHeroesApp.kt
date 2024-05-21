d0b78fc81493273a41529e38edd31212a4b16d00
chairul <chairul.amri@ebconnect.com>
2024-05-21 19:46:24 +0700

    [add] Codelab Sticky Header (create Character Header function & sorted by name and group based on initials)

--- lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/learn/jetpack/jetheroes/ui/JetHeroesApp.kt
+++ lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/learn/jetpack/jetheroes/ui/JetHeroesApp.kt
@@ -20,6 +20,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -29,9 +31,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -44,6 +48,10 @@ import kotlinx.coroutines.launch
 fun JetHeroesApp(
     modifier: Modifier = Modifier,
 ) {
+    val groupedHeroes = HeroesData.heroes
+        .sortedBy { it.name }
+        .groupBy { it.name[0] }
+
     Box(modifier = modifier) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
@@ -140,6 +148,27 @@ fun ScrollTopButton(
         Icon(
             imageVector = Icons.Default.KeyboardArrowUp,
             contentDescription = stringResource(id = R.string.scroll_to_top)
+        )
+    }
+}
+
+@Composable
+fun CharacterHeader(
+    char: Char,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+    ) {
+        Text(
+            text = char.toString(),
+            fontWeight = FontWeight.Black,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         )
     }
 }
