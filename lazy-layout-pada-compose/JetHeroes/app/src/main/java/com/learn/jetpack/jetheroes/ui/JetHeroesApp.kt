6e640d4817fed3e7a5af8b9cf63a3247f8aeb5b5
chairul <chairul.amri@ebconnect.com>
2024-05-21 17:28:49 +0700

    [update] UI JetHeroesApp

--- lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/learn/jetpack/jetheroes/ui/JetHeroesApp.kt
+++ lazy-layout-pada-compose/JetHeroes/app/src/main/java/com/learn/jetpack/jetheroes/ui/JetHeroesApp.kt
@@ -1,7 +1,22 @@
 package com.learn.jetpack.jetheroes.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.learn.jetpack.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
-fun JetHeroesApp() {
+fun JetHeroesApp(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesPreview() {
+    JetHeroesTheme {
+        JetHeroesApp()
+    }
 }
