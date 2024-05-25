a171b838a0d1bb522da7824ed7b50d28145f6699
chairul <chairul.amri@ebconnect.com>
2024-05-25 22:56:07 +0700

    [add] Route for details on the Screen class and sending data

--- navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/navigation/Screen.kt
+++ navigation-pada-compose/JetReward(solution)/app/src/main/java/com/dicoding/jetreward/ui/navigation/Screen.kt
@@ -4,4 +4,7 @@ sealed class Screen(val route: String) {
     object Home : Screen(route = "home")
     object Cart : Screen(route = "card")
     object Profile : Screen(route = "profile")
+    object DetailReward : Screen(route = "home{rewardId}"){
+        fun createRoute(rewardId: Long) = "home/$rewardId"
+    }
 }
